35cbc5a3e23c0549399b3f3a11fa8ff22f3f2a81
t0tinhaVitola <otaviobvoficial2005@gmail.com>
2026-05-20 15:32:35 -0300

    imported files from another computer

---
+++ Cache.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+struct Block{
+    bool val;
+    int tag;
+};
+
+class Cache{
+private:
+    Block** block;
+public:
+
+//cache_simulator 256 4 1 R 1 bin_100.bin
+//#####nome###sets####bsize###assoc####subst###flag_saida####bench####
+Cache(int argc, char**argv){
+    
+
+}
+
+
+
+
+} 
+
---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include stdio.h
+#include stdlib.h
+#include "Cache.hpp"
+
+
+int main(int argc, char** argv){
+    Cache cache = new Cache(argc, argv);
+}
